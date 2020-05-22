277af1865e4626a659b640ba9a7cc914b3eba0af
James Mace <jamesmace.pc@gmail.com>
2020-05-22 18:25:30 +0100

    variables

---
+++ hello-world.py
@@ -0,0 +1,19 @@
+# interger
+age=18
+# string
+town="manchester "
+# float
+money_in_pocket=20.1
+print(age,town,money_in_pocket)
+# list
+colours_in_rainbow=["blue","red","green"]
+print(colours_in_rainbow)
+
+county=" greater_manchester"
+address=town+county
+print(address)
+address+=county
+print(address) 
+# assignment operator
+age+=1
+print(age)
